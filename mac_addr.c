c9bb7e116546c83f7ea55a0ecf5291532f703925
Kent Chen <kent.chen@ubnt.com>
2015-02-16 18:38:16 +0800

    Update by adding get hwaddr through Sysfs

--- mac_addr.c
+++ mac_addr.c
@@ -27,6 +27,29 @@ void mac_addr(const char* name, unsigned char MAC_str[ETH_ABUFLEN])
 unsigned char eth0_MAC_address[ETH_ABUFLEN];
 
 int main(void) {
+
+	// 1st: ioctl -> SIOCGIFHWADDR
 	mac_addr("eth0",eth0_MAC_address);
 	puts(eth0_MAC_address);
+
+	// 2nd: sysfs
+	uint8_t mac[6];
+	char buf[32];
+	FILE *fp;
+	char hostname[63] = "hostname";
+
+	fp = fopen("/sys/class/net/eth0/address", "r");
+
+	if (fp)
+	{
+		if (fgets(buf, sizeof(buf), fp) > 0)
+		{
+			sscanf(buf, "%hhx:%hhx:%hhx:%hhx:%hhx:%hhx",
+					&mac[0], &mac[1], &mac[2], &mac[3], &mac[4], &mac[5]);
+		}
+		printf("%s-%02hhx%02hhx%02hhx%02hhx%02hhx%02hhx\n", hostname, mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+		fclose(fp);
+	}
+
+	return 0;
 }
